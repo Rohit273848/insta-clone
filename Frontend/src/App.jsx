0b96d09e1b0036f7afca7442f2d95719b52b4c66
Rohit Mahajan <rohitmahajan6942089@gamil.com>
2026-03-21 18:35:12 +0530

    login and register page simple html code done not scss

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,13 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router"; // import your router
+import {RouterProvider} from 'react-router';
+import AppRoutes from './AppRoutes';
+import './style.scss'
+
 
 function App() {
-  return <RouterProvider router={router} />;
+
+  return (
+   <AppRoutes/>
+  )
 }
 
-export default App;
+export default App
